c214e4ff726b9489f5b73f36fc541877913650b0
gharjare <gharjarechetan@gmail.com>
2022-10-12 15:35:46 +0530

    Employee Wages Check

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -11,16 +11,23 @@ namespace EmployeeWageProblem
         public static void CheckAttendence()
         {
             int Is_Full_Time = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empcheck = random.Next(2);
            if (empcheck == Is_Full_Time)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
            else
             {
                 Console.WriteLine("Employee is Absent ");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine( "EmployeeWage" +empWage);
         }
     }
 }
