055624f92b08d771b0a2e416e42e6db110174fef
gharjare <gharjarechetan@gmail.com>
2022-10-14 23:46:38 +0530

    working hours 100 or max days 20

--- EmployeeTimeAndWageCheck.cs
+++ EmployeeTimeAndWageCheck.cs
@@ -17,10 +17,16 @@ namespace EmployeeWageProblem
            const  int Is_Part_Time = 2;
             int EMP_RATE_PER_HOUR = 20;
             int NUM_OF_WORKING_DAYS = 3;
+            int MAX_HRS_IN_MONTH = 100;
+
+
 
             int empHrs = 0;
             int empWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
             {
 
 
@@ -41,10 +47,11 @@ namespace EmployeeWageProblem
                         empHrs = 0;
                         break;
                 }
+                totalEmpHrs = totalEmpHrs + empHrs;
+                Console.WriteLine("Day : " + totalWorkingDays + "Emp Hrs: " + empHrs);
 
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage for a Month: " + totalEmpWage);
         }
     }
 }
