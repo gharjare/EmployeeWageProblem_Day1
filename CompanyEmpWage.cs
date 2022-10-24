abd54849b4d27b33e795efb5cb1b861421094efc
gharjare <gharjarechetan@gmail.com>
2022-10-24 23:28:46 +0530

    added wages

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numofWorkingDays;
+        public int maxHoursePerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHour,int numofWorkingDays,int maxHoursePerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numofWorkingDays = numofWorkingDays;
+            this.maxHoursePerMonth = maxHoursePerMonth;
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public string tostring()
+        {
+            return "Total Emp Wage for company:" + this.company + "is:" + this.totalEmpWage;
+        }
+    }
+}
